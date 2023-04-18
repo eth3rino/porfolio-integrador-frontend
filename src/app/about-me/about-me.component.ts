bfbc4d9252cc08616d77c9a70e44a5e4a2cfba69
eth3rino <elianmeaca@gmail.com>
2023-04-17 23:03:55 -0300

    Advances in about-me page

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent {
-
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
+  }
 }
